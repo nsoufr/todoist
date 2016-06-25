a02fbec7b04a66131d2050eef19821d2980d4c30
Nando Sousa <nandosousafr@gmail.com>
2016-06-25 13:50:15 -0300

    adds docs for sync/2

--- lib/todoist.ex
+++ lib/todoist.ex
@@ -1,9 +1,20 @@
 defmodule Todoist do
   import Todoist.Client
 
+  @doc """
+  Executes request over a client.
+
+  ## Examples
+
+      iex> {:ok, cli} = Client.new("my_token")
+      iex> request = %ReadRequest{resource_types: ["projects"]}
+      iex> sync(cli, request)
+      %{"items" => ...}
+  """
+  @spec sync(Todoist.Client.t, Todoist.Request.t) :: map
   def sync(cli, request) do
     response = do_request(cli, request)
-    Poison.Parser.parse!(response.body)
+    json(response.body)
   end
 
   defp json(body), do: Poison.Parser.parse!(body)
